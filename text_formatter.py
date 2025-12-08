28ddcc54c45bfe1dba4ea9695ded56e5334da06d
Daniel Zema <fenton@chalmers.se>
2025-12-08 10:34:55 +0100

    Added left and middle text padding

--- text_formatter.py
+++ text_formatter.py
@@ -1,4 +1,4 @@
-MAX_WIDTH = 32
+MAX_WIDTH = 30
 
 def format_test_width():
     lines = []
@@ -8,3 +8,30 @@ def format_test_width():
 
 def test_printer_width():
     return format_test_width()
+
+def top_bottom_bordet(): 
+    print("+" + "-" * MAX_WIDTH + "+")
+
+def empty_line():
+    print("|" + " " * MAX_WIDTH + "|")
+
+def padded_text_left(text):
+    print("| " + text + " " * (MAX_WIDTH - len(text) - 1) + "|")
+
+def padded_text_middle(text):
+    total_padding = MAX_WIDTH - len(text)
+    left_padding = total_padding // 2
+    right_padding = total_padding - left_padding
+    print("|" + " " * left_padding + text + " " * right_padding + "|")
+
+def print_header_single_task():
+    top_bottom_bordet()
+    empty_line()
+    padded_text_left("Hello")
+    padded_text_middle("Hello")
+
+
+
+if __name__ == "__main__":
+    print_header_single_task()
+    # print(len(""))
